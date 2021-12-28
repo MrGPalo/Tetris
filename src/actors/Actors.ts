53166db3b546598a3d8ae164b1e8cc9d40fa2ae9
Gibson <gibson.palo@gmail.com>
2021-12-28 15:46:51 +0100

    types added

--- src/actors/Actors.ts
+++ src/actors/Actors.ts
@@ -1,1 +1,11 @@
+export interface IActor {
+	position: number;
+	update: (delta: number) => void;
+	draw: (delta: number, ctx: CanvasRenderingContext2D) => void;
+}
 
+export class Actor implements IActor {
+	position: ;
+	constructor(position: Point);
+	
+}
